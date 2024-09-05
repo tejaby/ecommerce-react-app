aab4347e38fea9485570e1924aedea8d79db90a3
tejaby <yostintejaxun20@gmail.com>
2024-09-05 01:57:05 -0600

    Add service and custom hook for fetching products

---
+++ src/hooks/useFetchService.js
@@ -0,0 +1,27 @@
+// react
+import { useState, useEffect } from "react";
+
+export const useFetchService = (service, token, category = null) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!token) return;
+      setLoading(true);
+      try {
+        const response = await service(token, category);
+        setData(response.data);
+      } catch (err) {
+        setError(err.data.error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  return { data, error, loading };
+};
---
+++ src/services/productService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const url = "http://localhost:3000/api/products";
+
+export const getProducts = async (token, category) => {
+  try {
+    const queryString = category ? `?category=${category}` : "";
+    const fullUrl = `${url}${queryString}`;
+
+    const response = await axios.get(`${fullUrl}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    throw err.response;
+  }
+};
